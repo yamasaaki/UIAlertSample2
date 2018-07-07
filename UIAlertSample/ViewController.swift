78ca286807cb55f9f34d29ba668b4e48e6a00901
yamasaaki <myamasaaki@gmail.com>
2018-07-07 11:14:37 +0900

    Initial Commit

---
+++ UIAlertSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIAlertSample
+//
+//  Created by Yamada Masaaki on 2018/07/07.
+//  Copyright © 2018年 Yamada Masaaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
